e5e0e86ed2b9fcbe2d55448365c5fbc5ec2c3249
Leandro M. Peralta <argenkiwi@hotmail.com>
2025-12-21 07:17:54 +1300

    Simplify retain molecule definition.

--- app/src/main/java/com/gethomsefe/arch26/retainMolecule.kt
+++ app/src/main/java/com/gethomsefe/arch26/retainMolecule.kt
@@ -3,15 +3,18 @@ package com.gethomsefe.arch26
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.retain.retain
 import app.cash.molecule.RecompositionMode
+import app.cash.molecule.SnapshotNotifier
 import app.cash.molecule.launchMolecule
 import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
 
 @Composable
 context(scope: CoroutineScope)
-fun <T> retainMolecule(vararg keys: Any?, body: @Composable () -> T) = retain(*keys) {
-    scope.launchMolecule(
-        mode = RecompositionMode.ContextClock,
-        context = scope.coroutineContext,
-        body = body
-    )
+fun <T> retainMolecule(
+    vararg keys: Any?,
+    mode: RecompositionMode = RecompositionMode.ContextClock,
+    context: CoroutineContext = scope.coroutineContext,
+    body: @Composable () -> T
+) = retain(*keys) {
+    scope.launchMolecule(mode, context, body =  body)
 }
