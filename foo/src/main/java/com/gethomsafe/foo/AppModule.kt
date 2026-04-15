ddbe3dbb0b03eac0e783df7f0df3952d41872e9a
Leandro Peralta <leandro.peralta@gethomesafe.com>
2026-04-15 15:02:59 +1200

    Foo: simplify DI.

--- foo/src/main/java/com/gethomsafe/foo/AppModule.kt
+++ foo/src/main/java/com/gethomsafe/foo/AppModule.kt
@@ -1,8 +1,8 @@
-package com.gethomsafe.foo.helloworld
+package com.gethomsafe.foo
 
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
 @Module
 @ComponentScan
-class HelloWorldModule
+class AppModule
